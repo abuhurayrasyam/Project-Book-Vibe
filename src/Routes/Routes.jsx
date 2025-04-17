86e5d5e398efd91dc4d3ed05cacbeaa3322f1b58
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-04-17 21:45:09 +0600

    Home Component Added in Children and This Children Showing on Roots Outlet

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,19 @@ import {
 } from "react-router";
 import Root from '../pages/Root/Root';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from './Home/Home';
 
 export const router = createBrowserRouter([
     {
       path: "/",
       Component: Root,
-      errorElement: <ErrorPage></ErrorPage>
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+      ]
     },
   ]);
